f742d85314a6d8cd7aa413180aa46ee28d46a15b
SrimanTetali <155832444+SrimanTetali@users.noreply.github.com>
2024-07-13 08:45:54 +0530

    Add files via upload

---
+++ combinational_sum_4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+
+        dp = [1] + [0] * target
+
+        for i in range(1, target + 1):
+            for n in nums:
+                if n <= i:
+                    dp[i] += dp[i-n]
+        
+        return dp[-1]
